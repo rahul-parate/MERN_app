cfa7321152f020f99ef1b72ba29d4dfa2a18a201
rahul-parate <rahulvitparate@gmail.com>
2018-02-27 00:50:42 +0530

    working

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ var app = express();
 
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb://heroku_0nrxvl6q:d6j382l02lrpf7bfmhq56qdo92@ds249818.mlab.com:49818/heroku_0nrxvl6q');
-  // .then(() =>  console.log('connection succesful'))
-  // .catch((err) => console.error(err));
+mongoose.connect('mongodb://heroku_0nrxvl6q:d6j382l02lrpf7bfmhq56qdo92@ds249818.mlab.com:49818/heroku_0nrxvl6q/shopping')
+  .then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
 
 
 app.use(logger('dev'));
@@ -21,6 +21,12 @@ app.use(express.static(path.join(__dirname, 'build')));
 // app.set('view engine', 'html');
 app.use('/api/product', product);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // error handler
 app.use(function(err, req, res, next) {
