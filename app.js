aabd9a102485864871bd2c5ddbbf3f10a477f94e
rahul-parate <rahulvitparate@gmail.com>
2018-02-27 00:40:15 +0530

    mongoose

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ var app = express();
 
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb://heroku_0nrxvl6q:d6j382l02lrpf7bfmhq56qdo92@ds249818.mlab.com:49818/heroku_0nrxvl6q')
-  .then(() =>  console.log('connection succesful'))
-  .catch((err) => console.error(err));
+mongoose.connect('mongodb://heroku_0nrxvl6q:d6j382l02lrpf7bfmhq56qdo92@ds249818.mlab.com:49818/heroku_0nrxvl6q');
+  // .then(() =>  console.log('connection succesful'))
+  // .catch((err) => console.error(err));
 
 
 app.use(logger('dev'));
